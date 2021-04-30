8d1d57c821fc16b6711ca2f4e347f31f9b0b19a6
jakeFromm <jacobfromm@gmail.com>
2021-04-30 12:30:02 -0400

    randomizer button working

--- src/components/RecordSelector.js
+++ src/components/RecordSelector.js
@@ -1,17 +1,22 @@
-import React from 'react'
+import React, {useState} from 'react'
 import _ from 'lodash'
 
 export default function RecordSelector(props) {
     const records = props.records
-    var randomRecordObj = _.sample(records)
-    // console.log ("random record", randomRecordObj)
+    let randomRecordObj = _.sample(records)
+    const [currentRecord, setCurrentRecord] = useState(randomRecordObj)
 
-    var randomRecord = props.records[0];
+
+    const clickHandler = () => {
+        setCurrentRecord(_.sample(records))
+        return currentRecord
+    }
     console.log("random record", randomRecordObj)
     return(
         <div className="records-container">
             <h3>Today you should listen to...</h3>
-            <h4><em>{randomRecordObj.name}</em> by {randomRecordObj.artist}</h4>
+            <h4><em>{currentRecord.name}</em> by {currentRecord.artist}</h4>
+            <button id="selection-button" onClick={clickHandler}>click for a different record</button>
         </div>
     )
 }
